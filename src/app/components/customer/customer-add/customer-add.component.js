3f0a09f0025e914a238bf0acb9206043b8dfca4f
vnguyener <its.vnguyen@gmail.com>
2016-07-21 01:21:30 -0500

    added room managing section in add form

--- src/app/components/customer/customer-add/customer-add.component.js
+++ src/app/components/customer/customer-add/customer-add.component.js
@@ -29,14 +29,31 @@ function CustomerAddController(customerAddService) {
     };
 
     self.room = {
-        size: 0,
-        numWindows: 0,
-        numStory: 0
+        size: null,
+        numWindows: null,
+        numStory: null
     };
 
-    var rooms = [];
+    self.rooms = [];
     self.options = [1,2,3,4,5,6];
     self.acTypes = ['Central Air', 'Window Unit', 'Mini Split', 'Other'];
     self.heatTypes = ['Furnace', 'Boiler', 'Heat Pump', 'Gas', 'Other'];
 
+
+    self.clearRoomForm = function() {
+        self.room.size = null;
+        self.room.numWindows = null;
+        self.room.numStory = null;
+    }
+
+    self.addRoom = function(room) {
+        self.rooms.push(room);
+        self.clearRoomForm();
+        console.log(self.rooms);
+    }
+
+    self.removeRoom = function(index) {
+        self.rooms.splice(index, 1);
+        console.log(self.rooms);
+    }
 }
